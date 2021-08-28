fixing data type after switching to quaternions

open_precision/sensor_adapters/bno055_aos_adapter.py
@@ -30,7 +30,7 @@ class Bno055AosAdapter(open_precision.core.sensors.absolute_orientation_sensor):
     @property
     def orientation(self) -> Quaternion:
         """returns an orientation quaternion"""
-        return np.array(self.sensor.euler)
+        return Quaternion(self.sensor.euler)
 
     @property
     def gravity(self) -> np.ndarray: