d37da73581187b7d38548821573a8206fd1c2214
larsjaeger <lars@jgr.one>
2021-09-05 16:30:30 +0200

    fixing logic

--- open_precision/core/sensor_manager.py
+++ open_precision/core/sensor_manager.py
@@ -19,13 +19,14 @@ class SensorManager:
         """
         print(f'Looking for plugins under package {self.plugin_dir}')
         sensor_types = utils.get_classes_in_package('open_precision.core.interfaces.sensor_types')
-        for (_, c) in utils.get_classes_in_package(self.plugin_dir):
+        plugin_classes = utils.get_classes_in_package(self.plugin_dir)
+        for (_, c) in plugin_classes:
             # Only add classes that are a sub class of plugin interfaces, but NOT an interface itself
             for (_, sensor_type) in sensor_types:
                 if not issubclass(sensor_type, ABC):
                     break
 
                 if issubclass(c, sensor_type) and (c is not sensor_type):
-                    #print(f'    Found plugin class: {c.__module__}.{c.__name__} is subclass of {sensor_type}')
+                    print(f'    Found plugin class: {c.__module__}.{c.__name__} is subclass of {sensor_type}')
                     self.plugins.append(c)
         print(f'blah {self.plugins}')
