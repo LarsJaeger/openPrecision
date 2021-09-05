833c075d4d82dcea245167bac023428078fa3e58
larsjaeger <lars@jgr.one>
2021-09-05 16:26:22 +0200

    fixing syntax

--- open_precision/core/sensor_manager.py
+++ open_precision/core/sensor_manager.py
@@ -28,7 +28,7 @@ class SensorManager:
                 if issubclass(sensor_type, ABC):
                     break
 
-                if issubclass(c, sensor_type) and (c is not sensor_type) and (c is not ):
+                if issubclass(c, sensor_type) and (c is not sensor_type):
                     print(f'    Found plugin class: {c.__module__}.{c.__name__} is subclass of {sensor_type}')
                     self.plugins.append(c)
         print(f'blah {self.plugins}')
