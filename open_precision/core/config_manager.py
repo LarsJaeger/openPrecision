53a97c85783de2c853df12a7d12a3537d1cfec34
larsjaeger <larsjgr@gmail.com>
2021-11-14 15:11:06 +0100

    improved version of config manager

--- open_precision/core/config_manager.py
+++ open_precision/core/config_manager.py
@@ -1,5 +1,7 @@
 import atexit
 
+from pydotdict import DotDict
+from ruamel import yaml
 from ruamel.yaml import YAML, CommentedMap, RoundTripLoader, RoundTripDumper
 from open_precision import utils
 
@@ -22,16 +24,10 @@ class ConfigManager:
         self._save_config_file()
 
     def register_value(self, origin_object: object, value_name: str, value: any, comment: str = None):
-        address = type(origin_object).__name__
-        if value_name is not None:
-            address += '.' + value_name
-        self._pack_val(value, address.split(sep='.'), self._config, comment=comment)
+        self._config[type(origin_object).__name__].insert(-1, value_name, value, comment=comment)
 
-    def get_value(self, origin_object: object, value_name: str):
-        address = type(origin_object).__name__
-        if value_name is not None:
-            address += '.' + value_name
-        return self._unpack_val(address.split(sep='.'), self._config)
+    def get_config(self, origin_object: object):
+        return DotDict(self._config[type(origin_object).__name__])
 
     def _load_config_file(self):
         print('[LOG]: loading config file')
@@ -43,20 +39,3 @@ class ConfigManager:
         print('[LOG]: saving config file')
         with open(self._config_path, 'r+') as config_file_stream:
             YAML().dump(self._config, stream=config_file_stream)
-
-    def _unpack_val(self, list_address: list, config: CommentedMap):
-        return config.get(list_address[0]) if len(list_address) == 1 else self._unpack_val(list_address[1:],
-                                                                                       config[list_address[0]])
-
-    def _pack_val(self, value, list_address: list, config: CommentedMap, comment: str = None):
-        print('a' + str(list_address))
-        print('a type' + str(config))
-        print('b' + str(value))
-        if len(list_address) == 1:
-            if config.get(list_address[0]) is not None:
-                config[list_address[0]] = value
-            config.insert(0, list_address[0], value, comment=comment)
-        else:
-            if config.get(list_address[0]) is None:
-                config.insert(0, list_address[0], CommentedMap())
-            self._pack_val(value, list_address[1:], config.get(list_address[0]))
--- tests/config_manager_test.py
+++ tests/config_manager_test.py
@@ -8,7 +8,7 @@ class ConfigManagerTest(unittest.TestCase):
     def test_config_manager(self):
         conf = ConfigManager('../config.yml')
         conf.register_value(self, 'depend.some_config_value', 3, comment='should normally be equal to the sqrt of nine')
-        print('Wert: ' + str(conf.get_value(self)))
+        print('Wert: ' + str(conf.get_config(self)))
         self.assertEqual(False, False)  # add assertion here
 
 
