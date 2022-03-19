5d37b318b0a8d3409a63ef0cc534f0798e4dc27a
larsjaeger <lars@jgr.one>
2022-03-19 19:50:47 +0100

    fixing wrong message classes

--- externalTools/ublox_gps_fixed/ublox_gps.py
+++ externalTools/ublox_gps_fixed/ublox_gps.py
@@ -294,8 +294,8 @@ class UbloxGps(object):
         self.send_message(sp.NAV_CLS, self.nav_ms.get("HPPOSECEF"))
         parse_tool = core.Parser([sp.NAV_CLS])
         cls_name, msg_name, payload = parse_tool.receive_from(self.hard_port)
-        if str(type(payload)) != "HPPOSECEF":
-            print("wrong message type: " + str(type(payload)))
+        if cls_name != "HPPOSECEF":
+            print("wrong message type: " + cls_name)
             return None
         s_payload = self.scale_NAV_HPPOSECEF(payload)
         return s_payload
