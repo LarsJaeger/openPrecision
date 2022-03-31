try fixing Quaternion error

open_precision/plugins/sensor_wrappers/bno055_aos_adapter.py
@@ -56,7 +56,12 @@ class Bno055AosAdapter(AbsoluteOrientationSensor):
     def orientation(self) -> Quaternion:
         """returns an orientation quaternion"""
         print("quat:: " + str(self.sensor.quaternion))
-        return Quaternion(self.sensor.quaternion) if Quaternion._validate_number_sequence(self.sensor.quaternion, 4) else None
+        try:
+            [float(e) for e in self.sensor.quaternion]
+        except ValueError:
+            print("aahhh Fehler")
+            return None
+        return Quaternion(self.sensor.quaternion)
 
     @property
     def gravity(self) -> np.array: