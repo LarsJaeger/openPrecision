7dd631742dcf1c3dab67a2cb8416ce4de6288211
larsjaeger <lars@jgr.one>
2022-05-30 21:20:48 +0200

    remove redundant functions

--- open_precision/core/interfaces/sensor_types/basic_sensor.py
+++ open_precision/core/interfaces/sensor_types/basic_sensor.py
@@ -16,14 +16,3 @@ class BasicSensor(ABC):
     @abstractmethod
     def _cleanup(self):
         pass
-
-    @property
-    @abstractmethod
-    def is_calibrated(self) -> bool:
-        pass
-
-    @abstractmethod
-    def calibrate(self) -> bool:
-        """calibrate device, (depending on your implementation also set is_calibrated accordingly) and
-        return True if calibration succeeded"""
-        pass
