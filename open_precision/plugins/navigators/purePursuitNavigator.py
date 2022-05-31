133c7e710b6c3ba83c6f87b25c398d8241ecd2c1
larsjaeger <lars@jgr.one>
2022-05-31 20:25:37 +0200

    adding debug prints

--- open_precision/plugins/navigators/purePursuitNavigator.py
+++ open_precision/plugins/navigators/purePursuitNavigator.py
@@ -52,6 +52,7 @@ class PurePursuitNavigator(Navigator):
                 for waypoint_id in range(nr_of_waypoints):
                     if waypoint_id == 0:
                         # check from wp_id 0 to wp_id 1
+                        print("X")
                         loss = self.calc_line_error(current_position, path.waypoints[0], path.waypoints[1])
                         if smallest_loss > loss:
                             smallest_loss = loss
@@ -59,6 +60,7 @@ class PurePursuitNavigator(Navigator):
                             best_segment_target_waypoint = path.waypoints[1]
                     elif waypoint_id == nr_of_waypoints - 1:
                         # check from wp_id nr_of_waypoints - 1 to previous wp
+                        print("XX")
                         print(f"LOL {path.waypoints[nr_of_waypoints - 1].path.waypoints}")
                         loss = self.calc_line_error(current_position, path.waypoints[nr_of_waypoints - 1],
                                                     path.waypoints[nr_of_waypoints - 2])
@@ -68,12 +70,16 @@ class PurePursuitNavigator(Navigator):
                             best_segment_target_waypoint = path.waypoints[nr_of_waypoints - 2]
                     else:
                         # check both directions
+                        print("XXX")
                         loss = self.calc_line_error(current_position, path.waypoints[waypoint_id],
                                                     path.waypoints[waypoint_id + 1])
                         if smallest_loss > loss:
                             smallest_loss = loss
                             best_segment_base_waypoint = path.waypoints[waypoint_id]
                             best_segment_target_waypoint = path.waypoints[waypoint_id + 1]
+
+
+                        print("XXXX")
                         loss = self.calc_line_error(current_position, path.waypoints[waypoint_id],
                                                     path.waypoints[waypoint_id - 1])
                         if smallest_loss > loss:
