17b8e4f2504093747a88fb1d57aed39067a77bdf
larsjaeger <lars@jgr.one>
2022-05-31 18:14:12 +0200

    add np.seterr('raise') to tests

--- open_precision/utils.py
+++ open_precision/utils.py
@@ -134,6 +134,8 @@ def calc_distance(loc1: Location, loc2: Location) -> float:
 
 
 def calc_distance_to_line(loc1: Location, line_base_point: Location, line_direction: np.array) -> float:
+    print(f"A {np.linalg.norm(np.cross((loc1 - line_base_point).to_numpy(), line_direction))}")
+    print(f"A {np.linalg.norm(line_direction)}")
     return np.divide(np.linalg.norm(np.cross((loc1 - line_base_point).to_numpy(), line_direction)),
                      np.linalg.norm(line_direction))
 
