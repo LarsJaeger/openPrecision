f23f77ba8036e078b4979fce8a6049d23c02952f
lars <lars@jgr.one>
2022-08-19 20:23:53 +0200

    fix model list setters

--- open_precision/core/model/course.py
+++ open_precision/core/model/course.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import field
 from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import relationship, Mapped, mapped_column
@@ -21,14 +22,31 @@ class Course(DataModelBase, PersistenceModelBase):
 
     name: Mapped[str] = mapped_column(init=True, default=None)
     description: Mapped[str] = mapped_column(init=True, default=None, nullable=True)
-    paths: Mapped[list[Path]] = relationship(default_factory=list, back_populates='course')
-
-    #TODO setter for paths
+    paths: list[Path] = field(init=False, default_factory=list)
+    _paths: Mapped[list[Path]] = relationship(default_factory=list, back_populates='course')
 
     def add_path(self, path: Path):
         # check if Path has at least two waypoints
+        self._check_path(path)
+        path.course = self
+        self._paths.append(path)
+        return self
+
+    @property
+    def paths(self):
+        return self._paths
+
+    @paths.setter
+    def paths(self, paths: list[Path]):
+        if isinstance(paths, list):
+            for path in paths:
+                self._check_path(path)
+                path.course = self
+            self._paths = paths
+        else:
+            self._paths = []
+
+    @staticmethod
+    def _check_path(self, path: Path):
         if len(path.waypoints) < 2:
             raise NotAPathException(path)
-        path.course = self
-        self.paths.append(path)
-        return self
--- open_precision/core/model/path.py
+++ open_precision/core/model/path.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import field
 from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey
@@ -20,13 +21,25 @@ class Path(DataModelBase, PersistenceModelBase):
     id: Mapped[int] = mapped_column(init=False, default=None, primary_key=True)
 
     priority: Mapped[int] = mapped_column(init=True, default=0)
-    waypoints: Mapped[list[Waypoint]] = relationship(init=False, default_factory=list, back_populates='path')
+    waypoints: list[Waypoint] = field(init=False, default_factory=list)
+    _waypoints: Mapped[list[Waypoint]] = relationship(init=False, default_factory=list, repr=False, back_populates='path')
     course_id: Mapped[int] = mapped_column(ForeignKey("Courses.id"), init=False, repr=False, default=None)
     course: Mapped[Course] = relationship(default=None, init=False)
 
-    #TODO setter for waypoints
-
     def add_waypoint(self, waypoint: Waypoint) -> Path:
         waypoint.path = self
-        self.waypoints.append(waypoint)
+        self._waypoints.append(waypoint)
         return self
+
+    @property
+    def waypoints(self):
+        return self._waypoints
+
+    @waypoints.setter
+    def waypoints(self, waypoints: list[Waypoint]):
+        if isinstance(waypoints, list):
+            for waypoint in waypoints:
+                waypoint.path = self
+            self._waypoints = waypoints
+        else:
+            self._waypoints = []
